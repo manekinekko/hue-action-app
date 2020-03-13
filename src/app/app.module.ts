e31a878956c3ff4a14a808eedbfe370b29ef9e57
Wassim Chegham <github@wassim.dev>
2020-03-13 10:39:54 +0100

    chore: clean unused modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,12 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HueAppInfoComponent } from "./hue-app-info/hue-app-info.component";
-import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import { MatSelectModule } from "@angular/material/select";
-import { MatRadioModule } from "@angular/material/radio";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatDividerModule } from "@angular/material/divider";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
+
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { ServiceWorkerModule } from "@angular/service-worker";
@@ -24,14 +21,10 @@ import { environment } from "../environments/environment";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatInputModule,
     MatButtonModule,
     MatSelectModule,
-    MatRadioModule,
     MatCardModule,
     MatIconModule,
-    MatToolbarModule,
-    MatDividerModule,
     MatProgressBarModule,
     ReactiveFormsModule,
     HttpClientModule,
