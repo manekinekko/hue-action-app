d8af6dbb07ab285f7140c8367ee92fbac61eab2a
Wassim Chegham <github@wassim.dev>
2020-05-16 21:52:06 +0200

    build: update stamp template

--- scripts/stamp-build.sh
+++ scripts/stamp-build.sh
@@ -5,13 +5,10 @@ set -o pipefail
 
 if [ -z "$GITHUB_SHA" ]
 then
-   v=`git rev-parse --short HEAD`
+   version=`git rev-parse --short HEAD`
 else
-   v=`echo $GITHUB_SHA | cut -c1-8`
+   version=`echo $GITHUB_SHA | cut -c1-8`
 fi
-
-sha="$v"
-version="<a href='https:\/\/github.com\/manekinekko\/hue-action-app\/commit\/$sha'>#$sha<\/a>"
 
 ## replease _BUILD_HASH_ with the current build number
 perl -i -pe "s/_BUILD_HASH_/$version/g" dist/hue-action-app/index.html
