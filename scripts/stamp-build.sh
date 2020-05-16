aa1455a678309cb3084761104498649f0a19fc88
Wassim Chegham <github@wassim.dev>
2020-05-16 21:13:55 +0200

    build: update stamp script

--- scripts/stamp-build.sh
+++ scripts/stamp-build.sh
@@ -5,15 +5,13 @@ set -o pipefail
 
 if [ -z "$GITHUB_SHA" ]
 then
-   b=`git rev-parse --abbrev-ref HEAD`
    v=`git rev-parse --short HEAD`
-   sha="$b+sha.$v"
 else
    v=`echo $GITHUB_SHA | cut -c1-8`
-   sha="#$v"
 fi
 
-version="<a href='https:\/\/github.com\/manekinekko\/hue-action-app\/tree\/$sha'>$sha<\/a>"
+sha="$v"
+version="<a href='https:\/\/github.com\/manekinekko\/hue-action-app\/tree\/$sha'>#$sha<\/a>"
 
 ## replease _BUILD_HASH_ with the current build number
 perl -i -pe "s/_BUILD_HASH_/$version/g" dist/hue-action-app/index.html
