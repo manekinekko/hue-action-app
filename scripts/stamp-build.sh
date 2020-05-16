0af0d8bebf797831861174ea70afe1bfc8907946
Wassim Chegham <github@wassim.dev>
2020-05-16 18:25:49 +0200

    build: stamp commit url

--- scripts/stamp-build.sh
+++ scripts/stamp-build.sh
@@ -7,18 +7,20 @@ if [ -z "$GITHUB_SHA" ]
 then
    b=`git rev-parse --abbrev-ref HEAD`
    v=`git rev-parse --short HEAD`
-   version="$b+sha.$v"
+   sha="$b+sha.$v"
 else
    v=`echo $GITHUB_SHA | cut -c1-8`
-   version="#$v"
+   sha="#$v"
 fi
+
+version="<a href='https:\/\/github.com\/manekinekko\/hue-action-app\/tree\/$sha'>$sha<\/a>"
 
 ## replease _BUILD_HASH_ with the current build number
 perl -i -pe "s/_BUILD_HASH_/$version/g" dist/hue-action-app/index.html
 
 status=$?
 if [ $status -eq 0 ];then
-   echo ">> Build was stamped: $version"
+   echo ">> Build was stamped: $sha"
 else
    echo ">> Could not stamp this build!"
 fi
